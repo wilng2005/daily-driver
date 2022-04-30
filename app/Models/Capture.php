d7ba2399f2e34fb81114b9fd28503e548d9bca37
wilng2005 <wil.ng.2005@gmail.com>
2022-04-30 12:46:03 +0800

    Adding in action log.

--- app/Models/Capture.php
+++ app/Models/Capture.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Nova\Actions\Actionable;
 
 class Capture extends Model
 {
     use HasFactory;
     use SoftDeletes;
-
+    use Actionable;
 }
