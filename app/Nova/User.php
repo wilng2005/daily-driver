4d65325cca807c3213901b1c4147de7918957bd7
Wil Ng <wil.ng@lyteventures.com>
2022-09-19 18:07:23 +0800

    Created access control fields for user and capture resource.

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -7,6 +7,7 @@ use Illuminate\Validation\Rules;
 use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Select;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
@@ -62,6 +63,20 @@ class User extends Resource
                 ->onlyOnForms()
                 ->creationRules('required', Rules\Password::defaults())
                 ->updateRules('nullable', Rules\Password::defaults()),
+
+            Select::make('User Resource Access')->options([
+                'All'=>"All",
+                'Self'=>"Self",
+                'None'=>"None",
+            ])->rules('required'),
+
+            Select::make('Capture Resource Access')->options([
+                'All'=>"All",
+                'Self'=>"Self",
+                'None'=>"None",
+            ])->rules('required'),
+
+
         ];
     }
 
---
+++ database/migrations/2022_09_19_174522_update_users_table_to_include_users_access_column.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('user_resource_access', 100)->default("None");
+            $table->string('capture_resource_access', 100)->default("None");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user_resource_access');
+            $table->dropColumn('capture_resource_access');
+        });
+       
+    }
+};
