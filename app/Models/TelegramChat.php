093208d69fda7d4bfc1d4b60b4656ec18e952fdc
Wil Ng <wil.ng.2005@gmail.com>
2023-05-08 10:56:33 +0800

    Created telegram chats object storage.

---
+++ app/Models/TelegramChat.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TelegramChat extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['data'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+    ];
+}
--- app/Models/TelegramUpdate.php
+++ app/Models/TelegramUpdate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\TelegramChat;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use OpenAI\Laravel\Facades\OpenAI;
@@ -23,6 +24,15 @@ class TelegramUpdate extends Model
 
     public function extract_and_store_chat_and_message_details(){
         info("TelegramUpdate->extract_and_store_chat_and_message_details() start");
+        info($this->data);
+        
+        if(isset($this->data['message']['chat'])){
+            $telegram_chat = TelegramChat::updateOrCreate(
+                ['telegram_chat_id'=>$this->data['message']['chat']['id']],
+                ['data'=>$this->data['message']['chat']]
+            );
+        }
+        
         info("TelegramUpdate->extract_and_store_chat_and_message_details() end");
     }
 
