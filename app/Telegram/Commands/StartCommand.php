9a784d6e1c39dfde7dabd97446f9c455e8c6a764
Wil Ng <wil.ng.2005@gmail.com>
2023-06-02 23:29:17 +0800

    Implemented environment check for start message.

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -11,7 +11,11 @@ class StartCommand extends Command
 
     public function handle()
     {
-        $this->replyWithMessage([
+        //if this is staging environment, reply with staging message
+        if (app()->environment(['production'])) {
+            
+
+            $this->replyWithMessage([
             'text' => '
 This is an AI-powered journaling bot that is currently available for trial usage.
 
@@ -23,6 +27,15 @@ To find out more about the trial, please check out the trial briefing slides at
 https://drive.google.com/file/d/1PiEpiL-TO39kDX51P_-7ify7jMj--Rwt/view?usp=share_link
              
 Please contact me on telegram @wilng2005 if you encounter any issues, or have any further clarifications.',
-        ]);
+            ]);
+        } else {
+            $this->replyWithMessage([
+                'text' => '
+This bot is not for public usage. 
+
+If you are looking for an AI-powered journaling bot, please use t.me/GreaterThanTodayBot
+Have a nice day!'
+            ]);
+        }
     }
 }
