c7051d14e71a3052b86106dee17bf29628f5b4a0
Wil Ng <wil.ng.2005@gmail.com>
2023-06-27 17:01:55 +0800

    Fixing the start command opening message.

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -20,7 +20,7 @@ class StartCommand extends Command
             
 Try to be as reflective and specific as possible, as this would help you to establish clarity about your own thoughts and feelings.
             
-Once you are done, you can type /done to end the conversation and get a summary of your thoughts and feelings.',
+Once you are done, you can type /done to end the conversation.',
         ]);
     }
 }
