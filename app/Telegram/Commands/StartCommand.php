a944323e7090b126c809e50e63c2bdf4c67857cd
Wil Ng <wil.ng.2005@gmail.com>
2023-07-12 17:30:28 +0800

    Slight tweak to start description.

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -18,7 +18,7 @@ class StartCommand extends Command
         $this->replyWithMessage([
             'text' => 'Hi! This is a chat bot that asks helpful questions to help you reflect on difficult problems and emotions. Simply get started by typing /new and respond to the question prompts like you would with any normal conversation.
             
-Try to be as reflective and specific as possible, as this would help you to establish clarity about your own thoughts and feelings.
+Try to be as reflective and specific as possible, as this would help you to establish mental clarity about your own thoughts and feelings.
             
 Once you are done, you can type /done to end the conversation.',
         ]);
