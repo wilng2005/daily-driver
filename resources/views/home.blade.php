5f6a19294df00d48af252a3e8df4a138a418b6c9
Wil Ng <wil.ng.2005@gmail.com>
2023-09-11 18:57:29 +0800

    Updated website to actually talk about coaching.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.115.4">
-    <title>Greater Than Today</title>
+    <title>Greater than Today</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/blog/">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
@@ -186,7 +186,7 @@
         
       </div>
       <div class="col-4 text-center">
-        <a class="blog-header-logo text-body-emphasis text-decoration-none" href="#">Greater Than Today</a>
+        <a class="blog-header-logo text-body-emphasis text-decoration-none" href="#">Greater than Today</a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
       </div>
@@ -198,22 +198,23 @@
       <a class="nav-item nav-link link-body-emphasis" href="/about">About</a>
       <a class="nav-item nav-link link-body-emphasis active" href="#">Articles</a>
       <a class="nav-item nav-link link-body-emphasis" href="#">Services</a>
-      <a class="nav-item nav-link link-body-emphasis" href="#">Pricing</a>
       <a class="nav-item nav-link link-body-emphasis" href="#">Clarity Bot</a>
       <a class="nav-item nav-link link-body-emphasis" href="#">Contact us</a>
     </nav>
   </div>
 </div>
 
-<main class="container">
-  <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
-    <div class="col-lg-6 px-0">
-      <h1 class="display-4 ">Title of a longer featured blog post</h1>
-      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-      <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold">Continue reading...</a></p>
-    </div>
-  </div>
 
+<main class="container">
+  
+  <!-- <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary" style="background-image: url('images/mountain.png'); background-repeat: no-repeat; background-size: cover;">
+    <div class="col-lg-6 px-0">
+      <h1 class="display-4 ">What is Coaching?</h1>
+      <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
+    </div>
+  </div> -->
+    
+<!--
   <div class="row mb-2">
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
@@ -250,132 +251,48 @@
       </div>
     </div>
   </div>
-
+    -->
   <div class="row g-5">
     <div class="col-md-8">
-      <h3 class="pb-4 mb-4 fst-italic border-bottom">
+      <!-- <h3 class="pb-4 mb-4 fst-italic border-bottom">
         From the Firehose
-      </h3>
+      </h3> -->
 
       <article class="blog-post">
-        <h2 class="display-5 link-body-emphasis mb-1">Sample blog post</h2>
-        <p class="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
+        <figure class="figure">
+          <img src="{{ asset('images/hands.jpg') }}" class="img-fluid rounded" alt="Responsive image" style="width: 100%; height: auto;">
+          <figcaption class="figure-caption">Photo by <a href="https://unsplash.com/@chrisliverani?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Liverani</a> on <a href="https://unsplash.com/photos/9cd8qOgeNIY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+          </figcaption>
+        </figure>
+        
+        <h2 class="display-5 link-body-emphasis mb-1">What is Coaching?</h2>
+        <p class="blog-post-meta">September 11, 2023 by Wil</p>
+        <p>Coaching has emerged as a powerful tool for personal and professional growth, gaining immense popularity in recent years. Whether it be in sports, business, or life, coaching has become a go-to resource for individuals seeking guidance, support, and transformation in various aspects of their lives.</p>
 
-        <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, lists, tables, images, code, and more are all supported as expected.</p>
+
+
+        <h3>So, how exactly can coaching benefit me?</h3>
+        <p>At its core, coaching is a collaborative partnership between a coach and a client, aimed at unlocking potential and achieving desired outcomes. Unlike therapy or counseling, coaching primarily focuses on the present and future, rather than delving into past traumas or psychological issues. It is a forward-thinking approach that engages individuals in a proactive process of self-discovery, goal-setting, and action planning.</p>
+        <h4>Clarity and Goal Setting:</h4>
+        <p> One of the fundamental aspects of coaching is helping you gain clarity about what you truly desire. By asking thought-provoking questions and challenging assumptions, coaches help you delve deep into your aspirations and identify your core values. Once you have a clear vision, a coach will assist you in setting ambitious yet achievable goals that align with your values, propelling you towards success.</p>
+        <h4>Accountability and Motivation:</h4>
+        <p>Many people struggle with staying motivated and holding themselves accountable when striving for personal and professional growth. A coach plays a crucial role in providing support and encouragement, acting as your accountability partner. They ensure that you stay on track, maintain focus, and celebrate even the smallest wins along your journey, making the ultimate goal seem more attainable.</p>
+        <h4>Enhancing Skills and Implementation:</h4>
+        <p>Through coaching, individuals can tap into their strengths and develop new skills that can propel them forward. Coaches often help in identifying and leveraging abilities that may have previously gone unnoticed. Whether it be enhancing leadership skills, effective communication, time management, or decision-making, a coach provides guidance, resources, and techniques to empower you in implementing these skills within your unique context.</p>
+        <h4>Overcoming Limiting Beliefs and Obstacles: </h4>
+        <p>Coaching equips you with strategies to identify and overcome limiting beliefs that might hinder your progress. By challenging self-imposed barriers and negative thought patterns, coaches help you replace them with positive affirmations and growth mindsets, fostering resilience and enhancing confidence.</p>
+        <h4>Work-Life Balance and Well-being: </h4>
+        <p>In today's fast-paced society, striking a balance between work, personal life, and overall well-being can be challenging. A coach assists in prioritizing self-care, setting boundaries, and finding harmony across various life domains, resulting in improved overall satisfaction and contentment.</p>
         <hr>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h2>Blockquotes</h2>
-        <p>This is an example blockquote in action:</p>
-        <blockquote class="blockquote">
-          <p>Quoted text goes here.</p>
-        </blockquote>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h3>Example lists</h3>
-        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout. This is an example unordered list:</p>
-        <ul>
-          <li>First list item</li>
-          <li>Second list item with a longer description</li>
-          <li>Third list item to close it out</li>
-        </ul>
-        <p>And this is an ordered list:</p>
-        <ol>
-          <li>First list item</li>
-          <li>Second list item with a longer description</li>
-          <li>Third list item to close it out</li>
-        </ol>
-        <p>And this is a definition list:</p>
-        <dl>
-          <dt>HyperText Markup Language (HTML)</dt>
-          <dd>The language used to describe and define the content of a Web page</dd>
-          <dt>Cascading Style Sheets (CSS)</dt>
-          <dd>Used to describe the appearance of Web content</dd>
-          <dt>JavaScript (JS)</dt>
-          <dd>The programming language used to build advanced Web sites and applications</dd>
-        </dl>
-        <h2>Inline HTML elements</h2>
-        <p>HTML defines a long list of available inline tags, a complete list of which can be found on the <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element">Mozilla Developer Network</a>.</p>
-        <ul>
-          <li><strong>To bold text</strong>, use <code class="language-plaintext highlighter-rouge">&lt;strong&gt;</code>.</li>
-          <li><em>To italicize text</em>, use <code class="language-plaintext highlighter-rouge">&lt;em&gt;</code>.</li>
-          <li>Abbreviations, like <abbr title="HyperText Markup Language">HTML</abbr> should use <code class="language-plaintext highlighter-rouge">&lt;abbr&gt;</code>, with an optional <code class="language-plaintext highlighter-rouge">title</code> attribute for the full phrase.</li>
-          <li>Citations, like <cite>— Mark Otto</cite>, should use <code class="language-plaintext highlighter-rouge">&lt;cite&gt;</code>.</li>
-          <li><del>Deleted</del> text should use <code class="language-plaintext highlighter-rouge">&lt;del&gt;</code> and <ins>inserted</ins> text should use <code class="language-plaintext highlighter-rouge">&lt;ins&gt;</code>.</li>
-          <li>Superscript <sup>text</sup> uses <code class="language-plaintext highlighter-rouge">&lt;sup&gt;</code> and subscript <sub>text</sub> uses <code class="language-plaintext highlighter-rouge">&lt;sub&gt;</code>.</li>
-        </ul>
-        <p>Most of these elements are styled by browsers with few modifications on our part.</p>
-        <h2>Heading</h2>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h3>Sub-heading</h3>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <pre><code>Example code block</code></pre>
-        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
+        <p>Remember, coaching is a personalized and dynamic process that caters to your specific needs and objectives. It offers a safe and non-judgmental space for self-reflection, growth, and transformation. So, if you are someone who yearns for progress, desires to overcome obstacles, and is committed to self-improvement, coaching may be the missing piece in your journey towards success and fulfillment.</p>
+
+       
       </article>
 
-      <article class="blog-post">
-        <h2 class="display-5 link-body-emphasis mb-1">Another blog post</h2>
-        <p class="blog-post-meta">December 23, 2020 by <a href="#">Jacob</a></p>
+  
 
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <blockquote>
-          <p>Longer quote goes here, maybe with some <strong>emphasized text</strong> in the middle of it.</p>
-        </blockquote>
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <h3>Example table</h3>
-        <p>And don't forget about tables in these posts:</p>
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Upvotes</th>
-              <th>Downvotes</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Alice</td>
-              <td>10</td>
-              <td>11</td>
-            </tr>
-            <tr>
-              <td>Bob</td>
-              <td>4</td>
-              <td>3</td>
-            </tr>
-            <tr>
-              <td>Charlie</td>
-              <td>7</td>
-              <td>9</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td>Totals</td>
-              <td>21</td>
-              <td>23</td>
-            </tr>
-          </tfoot>
-        </table>
 
-        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
-      </article>
 
-      <article class="blog-post">
-        <h2 class="display-5 link-body-emphasis mb-1">New feature</h2>
-        <p class="blog-post-meta">December 14, 2020 by <a href="#">Chris</a></p>
-
-        <p>This is some additional paragraph placeholder content. It has been written to fill the available space and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the demonstration flowing, so be on the lookout for this exact same string of text.</p>
-        <ul>
-          <li>First list item</li>
-          <li>Second list item with a longer description</li>
-          <li>Third list item to close it out</li>
-        </ul>
-        <p>This is some additional paragraph placeholder content. It's a slightly shorter version of the other highly repetitive body text used throughout.</p>
-      </article>
-
-      <nav class="blog-pagination" aria-label="Pagination">
-        <a class="btn btn-outline-primary rounded-pill" href="#">Older</a>
-        <a class="btn btn-outline-secondary rounded-pill disabled" aria-disabled="true">Newer</a>
-      </nav>
 
     </div>
 
