29ccf881a19604e80c6dc95e6f9862fb46324892
Wil Ng <wil.ng.2005@gmail.com>
2024-02-26 11:18:38 +0800

    Trying to improve how posts are displayed on tag page.

--- resources/views/tag.blade.php
+++ resources/views/tag.blade.php
@@ -205,7 +205,30 @@
 
 
 <main class="container">
-<div class="row mb-2">
+  @if($tag->posts && $tag->posts->count() > 0)
+  <div class="row mb-2">
+    @foreach($tag->posts as $post)
+      <div class="col-md-6">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
+            <h3 class="mb-0">{{ $post->title }}</h3>
+            <div class="mb-1 text-body-secondary">Nov 12</div>
+            <p class="card-text mb-auto"> {{ Str::words($post->content, 10, "...") }}</p>
+            <a href="/post/{{ $post->slug }}" class="icon-link gap-1 icon-link-hover stretched-link">
+              Continue reading
+              <svg class="bi"><use xlink:href="#chevron-right"/></svg>
+            </a>
+          </div>
+          <div class="col-auto d-none d-lg-block">
+            <img src="{{Storage::temporaryUrl($tag->image_file, now()->addMinutes(5)) }}" class="bd-placeholder-img img-fluid rounded" alt="Responsive image" style="width: 200px; height: 250px; object-fit: cover;">
+          </div>
+        </div>
+      </div>
+    @endforeach
+  </div>
+  @endif
+  <div class="row mb-2">
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
