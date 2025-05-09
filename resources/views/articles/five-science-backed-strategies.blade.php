4e317d70dedef91f3a258b3fc48f7c85af9af635
Wil Ng <wil.ng.2005@gmail.com>
2025-05-09 11:59:03 +0800

    Update Dusk workflow step to override exit code 1 unless real failures are present

--- resources/views/articles/five-science-backed-strategies.blade.php
+++ resources/views/articles/five-science-backed-strategies.blade.php
@@ -140,7 +140,7 @@
             <div class="container">
                 <div class="d-flex align-items-center row">
                 <div class="col-md-5">
-                        <img src="{{asset('images/asset-6v2.png')}}" class="icon-image full" alt="">
+                        <img src="{{asset('images/exercise-1.png')}}" class="icon-image full" alt="">
                     </div> 
                     <div class="col-md-7 pe-5">
                         <div>
