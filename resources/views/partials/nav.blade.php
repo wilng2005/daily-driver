f2af715ef8dbc8ed684e9145b749b229605cc085
Wil Ng <wil.ng.2005@gmail.com>
2025-05-18 17:56:50 +0800

    Added new logos,

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-yellow pb-3">
         <div class="container">
             <a class="navbar-brand" href="#">
-               &nbsp; <!-- <img src="{{asset('images/logo.png')}}" class="image" alt=""> -->
+               &nbsp;  <img src="{{asset('images/logo-v4.png')}}" class="image" alt=""> 
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
