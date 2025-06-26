7ec20fa4bc21a9a44dd2f48be68f9b0e3597ccc9
Wil Ng <wil.ng.2005@gmail.com>
2025-06-26 15:24:24 +0800

    Improving CTA

---
+++ resources/views/partials/cta-b.blade.php
@@ -0,0 +1,38 @@
+<section class="background--blue">
+        <div class="container">
+            <div class="d-flex align-items-center row">
+
+                <div class="col-md-7 pe-5">
+                    <div>
+                        <h2>Ready to make a change?</h2>         
+                        <br/>
+
+                        <p>You’re successful—or at least, others say you are.
+                        But something feels off.</p>
+
+                        <p>Your team is steady. Your career is predictable. Yet, the path you’re on doesn’t seem to lead where you want to be. Complacency is creeping in. The future looks “safe,” but not exciting. Or maybe you sense that one day, things could crumble suddenly.</p>
+
+                        <p>It’s easy to ignore the warning signs when everything seems fine. Real growth happens when you question the status quo and act before crisis hits.</p>
+
+                        <p>Discover if coaching is your catalyst for change—risk-free, with no obligation.</p>
+                        <p>In just one free session, you’ll:</p>
+                        <ul>
+                            <li>Unpack your unwritten goals and hopes for the future</li>
+                            <li>Gain clarity on your current reality—your environment, opportunities, and threats</li>
+                            <li>Get a clear path forward to realignment, momentum, and impact</li>
+                        </ul>
+                        <p>Whether you’re a founder, team lead, or ambitious professional, this isn’t a sales pitch. It’s a powerful 1-hour strategy session designed to spark clarity and action.</p>
+                        <blockquote>No fluff. No obligations. Just a chance to check in and reset your trajectory.</blockquote>
+                        <a href="{{ url('/redirect-to-cal?target=https://cal.com/wilng/free-coaching-session') }}" target="_blank"
+                        class="read-more">Get Started with a Free Session &rarr;
+                        </a>
+                    </div>
+                </div>
+     
+                <div class="col-md-5">
+                    <img src="{{ asset('images/bored-01.png') }}" class="icon-image full" alt="">
+                </div>
+            
+            </div>
+        </div>
+    </section>
